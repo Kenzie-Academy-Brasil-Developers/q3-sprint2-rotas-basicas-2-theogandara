4c04fcc0544b509a1de28dd0853c28e1ca4f8c13
Théo Gandara <theogandara@gmail.com>
2022-02-24 10:41:30 -0300

    feat: added routes and lib datetime

--- rotas_basicas/app.py
+++ rotas_basicas/app.py
@@ -1,1 +1,26 @@
-# Seu código aqui
+from flask import Flask
+from datetime import datetime as dt
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    return {"data": "Hello Flask!"}, 200
+
+
+@app.get("/current_datetime")
+def current_datetime():
+
+    message = "Bom dia!"
+    current_time = dt.now()
+
+    if current_time.hour > 12:
+        message = "Boa tarde!"
+
+    elif current_time.hour > 18:
+        message = "Boa noite!"
+
+    show_date = current_time.strftime("%d/%m/%Y %I:%M:%S %p")
+
+    return {'current_datetime': show_date, 'message': message}, 200
